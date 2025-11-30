04268c48e4070b51e16c812494771e2479ef6f5b
Luiza Rocha <luizaorocha2009@gmail.com>
2025-11-30 13:11:14 -0300

    sim

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import express, { request, response } from "express"
 import cors from "cors"
 import mysql2 from "mysql2"
 
-const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = process.env
+const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD} = process.env
 
-const app = express()
+const app = express ()
 const port = 3333
 
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (request, response) => {
-    const selectCommand = "SELECT nome, email FROM luizarocha_02mb"
+app.get("/", (request, response) =>{
+    const selectCommand = "SELECT name, email FROM luizarocha_02mb"
 
-    database.query(selectCommand, (error, users) => {
-        if (error) {
+    database.query(selectCommand, (error, users) =>{
+        if (error){
             console.log(error)
             return
         }
@@ -23,50 +23,26 @@ app.get("/", (request, response) => {
     })
 })
 
-app.post("/login", (request, response) => {
-    const { email, password } = request.body.user
-
-    const selectCommand = "SELECT * FROM luizarocha02mb WHERE email = ?"
-
-    database.query(selectCommand, [email], (error, user) => {
-        if (error) {
-            console.log(error)
-            return
-        }
-
-        if (user.length === 0 || user[0].password !== password) {
-            response.json({ message: "Usuário ou senha incorretos!" })
-            return
-        }
-
-        response.json({ id: user[0].id, nome: user[0].nome })
-    })
-})
-
-app.post("pontuacao", (request,response) => {
-
-})
-
 app.post("/cadastrar", (request, response) =>{
-    const { user } = request.body
-    console.log(user)
+    const {user} = request.body
+    console.log (user)
 
-    const insertCommand = `
-        INSERT INTO luizarocha_02mb(nome, email, password)
-        VALUES (?, ?, ?)
+    const insertCommand=`
+    INSERT INTO luizarocha_02mb (name, email, password)
+    VALUES (?, ?, ?)
     `
 
-    database.query(insertCommand, [user.name, user.email, user.password], (error) => {
-        if(error) {
+    database.query (insertCommand, [user.name, user.email, user.password], (error) =>{
+        if (error){
             console.log(error)
             return
-        }
+        } 
 
-            response.status(201).json({ message: "Usúario cadrastado com sucesso!" })
+    response.status(201).json({message: "Usuário cadastrado com sucesso!"})
     })
 })
 
-app.listen(port, () => {
+app.listen(port, () =>{
     console.log(`Server running on port ${port}!`)
 })
 
@@ -75,5 +51,7 @@ const database = mysql2.createPool({
     database: DB_NAME,
     user: DB_USER,
     password: DB_PASSWORD,
-    connectionLimit: 10
-})
+    connectionLimit: 11
+}
+
+)
